ef622867d28412a47bab23342087178f4631891f
jamithj <jamithj@gmail.com>
2017-06-25 18:53:44 +1000

    More unit tests

--- src/test/scala/com/board/agile/TestBoard.scala
+++ src/test/scala/com/board/agile/TestBoard.scala
@@ -12,8 +12,14 @@ class TestBoard extends FunSuite{
   val cards = Seq(Card("Title1", "Description1", 3, None), Card("Title2", "Description1", 5, None), Card("Title3", "Description1", 9, None), Card("Title4", "Description1", 16, None))
   var iteration = Iteration(1, columns, Nil, None, 0)
   val board = new Board(iteration)
+
   val card1 = cards.head
+  val card2 = cards(1)
+  val card3 = cards(2)
+  val card4 = cards(3)
   val columnStarting = columns.head
+  val columnInProgress = columns(1)
+  val columnDone = columns(2)
 
   test("agile board") {
     iteration = iteration.addCard(card1)
@@ -25,6 +31,24 @@ class TestBoard extends FunSuite{
     assert(iteration.lastMove == Some(card1, card1InStartingColumn))
     assert(iteration.cards.contains(card1InStartingColumn))
     assert(iteration.velocity == 0)
+
+    //Moving card2 to starting column
+    iteration = iteration.addCard(card2)
+    iteration = iteration.moveCard(card2, Some(columnStarting))
+    val card2InStartingColumn = card2.copy(currentColumn = Some(columnStarting))
+    assert(iteration.lastMove == Some(card2, card2InStartingColumn))
+    assert(iteration.cards.contains(card2InStartingColumn))
+    assert(iteration.velocity == 0)
+
+    //Moving card3 to starting column
+    iteration = iteration.addCard(card3)
+    iteration = iteration.moveCard(card3, Some(columnStarting))
+    val card3InStartingColumn = card3.copy(currentColumn = Some(columnStarting))
+    assert(iteration.lastMove == Some(card3, card3InStartingColumn))
+    assert(iteration.cards.contains(card3InStartingColumn))
+    assert(iteration.velocity == 0)
+
+
   }
 
 }
